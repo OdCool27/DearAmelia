2370bfdb441949ad82c0131846851b2584980597
Odane Collins <odanecollins@gmail.com>
2026-02-05 18:11:20 -0500

    Added email api

--- extra.js
+++ extra.js
@@ -12,3 +12,5 @@ envelope.addEventListener("click", () => {
 });
 
 
+
+
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const text = ["So, I wanted to do something that stood out. Maybe even sweep you off your feet a bit. So, I decided to do so in the best way I knew how - Building a website.",
-        "This website has been made from scratch - from the design, to the words. It was done so to remain authenticity and portray genuine emotions. So, I hope you enjoy!",
+        "This website has been made from scratch - from the design, to the words. It was done so to remain authentic and portray genuine emotions. So, I hope you enjoy!",
         "The time we've spent together, though only just a few months, has felt like years to me.",
         "It has been a privilege to experience your beauty, your kindness and hear your innermost thoughts,",
-        "which is something I would never take for granted.",
+        "which is something I've come to treasure and would never take for granted.",
         "Each day, I am in awe of the traits you've display, such as:",
         "your persistent optimism and faith in the face of trials,",
         "your accountability and willingness assess where you need to improve and work on it,",
         "your tendency to think about others", "Did I mention your beauty?", "You're very beautiful", "Like serious though...",
-        "Very Beautiful! 😍", "Muy Hermosa! ", "Big up Mom and Dad! They did their thing.", "But anyways, long story short - I think you're amazing!", "Each day, I am filled with gratitude for the day we started talking.",
-        "I thank God for our lives intersecting, and I hope it is intended for more than just a season.", "Now, I don't believe in celebrating valentines's day, so I won't ask you to be my valentine.", "But I do have another question:", "And I would be FULLY DUNCE if I didn't ask...",
-        `Would you do me the honor of being my girlfriend? (P.S - The 'No' button is just for decoration)`]
+        "Muy Hermosa! ", "Absolutely Gorgeous! 😍", "Big up Mom and Dad! They did their thing.", "But anyways, long story short - I think you're amazing!",
+        "Each day, I am filled with gratitude for the day we started talking.", "I thank God for our lives intersecting, and I hope it is intended for more than just a season.",
+        "Now, I don't believe in celebrating valentines's day, so I won't ask you to be my valentine.", "But I do have another question:", "And I would be fully dunce if I didn't ask...",
+        "Would you do me the honor of being my girlfriend?"]
 
 const gifs = ["media/1.gif", "media/2.gif", "media/3.gif", "media/4.gif", "media/5.gif"];
 const gifContainer = document.querySelector(".gif-container");
@@ -149,10 +150,10 @@ prev.addEventListener("click", function() {
 })
 
 // mouse
-noBtn.addEventListener("mouseenter", () => dodgeButton(noBtn));
+//noBtn.addEventListener("mouseenter", () => dodgeButton(noBtn));
 
 // touch
-noBtn.addEventListener("touchstart", () => dodgeButton(noBtn));
+//noBtn.addEventListener("touchstart", () => dodgeButton(noBtn));
 
 
 //Yes Button
